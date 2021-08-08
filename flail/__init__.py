6b397e1f6cdc91e7c061ae70ced7e52436fa2aa0
joncrall <jon.crall@kitware.com>
2021-08-08 11:39:34 -0400

    Add __version__

--- flail/__init__.py
+++ flail/__init__.py
@@ -2,11 +2,11 @@
 
 __all__ = ['_']
 
+__version__ = '0.1.0'
+
 _ = type('',(),dict(__getattr__=lambda s,_:s))()
 
-        
 
-    
 
 
 
--- setup.py
+++ setup.py
@@ -3,13 +3,40 @@ try:
 except ImportError:
     from distutils.core import setup
 
+
+def parse_version(fpath):
+    """
+    Statically parse the version number from a python file
+    """
+    import ast
+    import os
+    if not os.path.exists(fpath):
+        raise ValueError('fpath={!r} does not exist'.format(fpath))
+    with open(fpath, 'r') as file_:
+        sourcecode = file_.read()
+    pt = ast.parse(sourcecode)
+    class Finished(Exception):
+        pass
+    class VersionVisitor(ast.NodeVisitor):
+        def visit_Assign(self, node):
+            for target in node.targets:
+                if getattr(target, 'id', None) == '__version__':
+                    self.version = node.value.s
+                    raise Finished
+    visitor = VersionVisitor()
+    try:
+        visitor.visit(pt)
+    except Finished:
+        pass
+    return visitor.version
+
 setup(name = "flail",
             description="Flail - The Ball and Chain Decorator",
             long_description = """
 Decorators with more injuries
 """,
             license="""MIT""",
-            version = "0.0",
+            version = parse_version('flail/__init__.py'),
             author = "David Beazley",
             author_email = "dave@dabeaz.com",
             maintainer = "David Beazley",
